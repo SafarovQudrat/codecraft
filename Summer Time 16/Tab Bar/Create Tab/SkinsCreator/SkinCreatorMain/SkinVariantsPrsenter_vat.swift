7085735c418b5e0050ba103dc632c5f3aef198d6
Mirjaloliddin <murtazaev_26@icloud.com>
2024-06-29 14:06:22 +0500

    add skin

---
+++ Summer Time 16/Tab Bar/Create Tab/SkinsCreator/SkinCreatorMain/SkinVariantsPrsenter_vat.swift
@@ -0,0 +1,30 @@
+//
+//  SkinVariantsPrsenter_vat.swift
+//  Summer Time 16
+//
+//  Created by Diyorbek Xikmatullayev on 29/06/24.
+//  Copyright © 2024 app.soft-it. All rights reserved.
+//
+
+import UIKit
+
+class SkinVariantsPrsenter_vat: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Summer Time 16/Tab Bar/Create Tab/SkinsCreator/SkinCreatorMain/SkinVariantsViewController_vatr.swift
@@ -0,0 +1,9 @@
+//
+//  SkinVariantsViewController_vatr.swift
+//  Summer Time 16
+//
+//  Created by Diyorbek Xikmatullayev on 29/06/24.
+//  Copyright © 2024 app.soft-it. All rights reserved.
+//
+
+import Foundation
