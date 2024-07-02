d4a80a05a15d7b6d2afed751824b99baa8f8979e
Mirjaloliddin <murtazaev_26@icloud.com>
2024-07-02 11:46:00 +0500

    skin collection

---
+++ Summer Time 16/Tab Bar/Mods Tab/selectorCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  selectorCollectionViewCell.swift
+//  Summer Time 16
+//
+//  Created by Diyorbek Xikmatullayev on 01/07/24.
+//  Copyright © 2024 app.soft-it. All rights reserved.
+//
+
+import UIKit
+
+class selectorCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
